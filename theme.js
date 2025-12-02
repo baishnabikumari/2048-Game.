39e762a013fc9539c1695372f732dedc591cea26
baishnabikumari <baishnabi.5.06.2011@gmail.com>
2025-12-02 20:02:12 +0530

    bg(light) and bg(dark) both colors is inspired by milkyway cuz i am creating this game for miklyway only.

---
+++ theme.js
@@ -0,0 +1,10 @@
+// little theme toggle simple and clean.
+(() => {
+    const root = document.documentElement;
+    const themeBtn = document.getElementById('themeBtn');
+    const saved = localStorage.getItem('theme2048') || 'light';
+
+    function apply(t){
+
+    }
+})
